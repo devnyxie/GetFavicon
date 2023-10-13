cd1d207866fd9d1037c8b42bb1e399b123ba0657
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-10-13 15:11:48 +0200

    0.6.1

--- getFavicon/__init__.py
+++ getFavicon/__init__.py
@@ -7,34 +7,34 @@ import aiohttp
 import asyncio
 import lxml.html
 import timeout_decorator
-
+import requests
 headers = requests.utils.default_headers()
 headers.update(
     {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+        "Accept-Language": "en",
+        "Referer": "https://www.google.com",
+        "DNT": "1",
+        "Cache-Control": "max-age=0"
     }
 )
 
 async def async_get_favicon_from_sources(session, url, timeout, first_only):
     favicon_links = []
+    tasks = []
+
     try:
-        used_url = f"http://{tldextract.extract(url).fqdn}/"
-        async with session.get(used_url, headers=headers, timeout=timeout) as res:
+        async with session.get(url, timeout=timeout) as res:
             content = await res.text()
             html = lxml.html.fromstring(content)
 
             async def find_in_html_and_make_request(path):
                 links = html.xpath(path)
-                tasks = []
-
                 for link in links:
                     href = link.get('href')
                     if href:
-                        task = async_get_favicon_from_url(session, url, href, timeout, first_only)
+                        task = async_get_favicon_from_url(session, url, href)
                         tasks.append(task)
-                if tasks:
-                    results = await asyncio.gather(*tasks)
-                    favicon_links.extend([result for result in results if result is not None])
 
             sources = [
                 '//link[@rel="icon" or @rel="shortcut icon"]',
@@ -47,14 +47,28 @@ async def async_get_favicon_from_sources(session, url, timeout, first_only):
                 if first_only and favicon_links:
                     return favicon_links
                 await find_in_html_and_make_request(source)
+
+            tasks.append(check_favicon_ico(session, url))
+
+            if tasks:
+                    results = await asyncio.gather(*tasks)
+                    favicon_links.extend([result for result in results if result is not None])
             return favicon_links
     except requests.exceptions.RequestException as e:
         print(f"Error while fetching the page: {e}")
         return favicon_links
 
-async def async_get_favicon_from_url(session, base_url, href, timeout, first_only):
+async def check_favicon_ico(session, url):
+    async with session.get(urljoin(url, 'favicon.ico'), headers=headers) as res:
+        format, width, height = detect_image_format_and_size(await res.read())
+        if format:
+            return {"format": format.lower(), "width": width, "height": height, "url": urljoin(url, 'favicon.ico')}
+
+
+
+async def async_get_favicon_from_url(session, base_url, href):
     try:
-        async with session.get(urljoin(base_url, href), headers=headers, timeout=timeout) as res:
+        async with session.get(urljoin(base_url, href)) as res:
             format, width, height = detect_image_format_and_size(await res.read())
             if format:
                 return {"format": format.lower(), "width": width, "height": height, "url": urljoin(base_url, href) }
@@ -66,9 +80,10 @@ async def async_get_favicon_from_url(session, base_url, href, timeout, first_onl
 
 async def scan_async(domain_url: pydantic.HttpUrl, timeout=2, first_only = False):
     try:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers=headers) as session:
             domain = tldextract.extract(domain_url)
-            used_url = f"http://{domain.fqdn}/"
+            protocol = 'https://' if domain_url.startswith('https://') else 'http://'
+            used_url = f"{protocol}{domain.fqdn}/"
             async_get_favicon_task = async_get_favicon_from_sources(session, used_url, timeout, first_only)
             result = await asyncio.wait_for(async_get_favicon_task, timeout=timeout)
             return result
@@ -84,8 +99,8 @@ async def scan_async(domain_url: pydantic.HttpUrl, timeout=2, first_only = False
 def sync_get_favicon_from_sources(url, timeout, first_only):
     favicon_links = []
     try:
-        used_url = f"http://{tldextract.extract(url).fqdn}/"
-        response = requests.get(used_url, headers=headers, timeout=timeout)
+        # used_url = f"http://{tldextract.extract(url).fqdn}/"
+        response = requests.get(url, headers=headers, timeout=timeout)
         response.raise_for_status()
         content = response.text
         html = lxml.html.fromstring(content)
@@ -95,19 +110,20 @@ def sync_get_favicon_from_sources(url, timeout, first_only):
             for link in links:
                 href = link.get('href')
                 if href:
-                    response = requests.get(f"{urljoin(url, href)}", headers=headers, timeout=timeout)
+                    response = requests.get(urljoin(url, href))
                     format, width, height = detect_image_format_and_size(response.content)
                     if format:
                         favicon_links.append({"format": format.lower(), "width": width, "height": height, "url": urljoin(url, href)})
                         if first_only:
                             return favicon_links
-                    else:
-                        if first_only:
-                            return favicon_links
                 else:
                     if first_only:
                         return favicon_links
-
+            response = requests.get(urljoin(url, 'favicon.ico'))
+            format, width, height = detect_image_format_and_size(response.content)
+            if format:
+                favicon_links.append({"format": format.lower(), "width": width, "height": height, "url": urljoin(url, 'favicon.ico')})
+            return favicon_links
         sources = [
             '//link[@rel="icon" or @rel="shortcut icon"]',
             '//meta[@name="msapplication-TileImage"]',
@@ -128,7 +144,8 @@ def scan(domain_url: pydantic.HttpUrl, timeout = 2, first_only = False):
     def inner_timeout():
         try:
             domain = tldextract.extract(domain_url)
-            used_url = f"http://{domain.fqdn}/"
+            protocol = 'https://' if domain_url.startswith('https://') else 'http://'
+            used_url = f"{protocol}{domain.fqdn}/"
             links = sync_get_favicon_from_sources(used_url, timeout, first_only)
             return links
         except Exception as e:
--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ from setuptools import setup
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-# Read the content of Markdown file
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
 setup(
     name='getFavicon',
-    version='0.6.0',
+    version='0.6.1',
     author='Tim Afanasiev',
     author_email='timbusinez@gmail.com',
     description='A Python library to fetch favicons from URLs',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/talmkg/getFavicon',
@@ -24,10 +18,10 @@ setup(
         'tldextract==3.4.4',
         'pydantic==1.10.2',
         'pillow==10.0.0',
-        'aiohttp==3.8.5',
+        'aiohttp==3.8.6',
         'asyncio==3.4.3',
         'lxml==4.9.3',
-        'timeout-decorator==0.5.0'
+        'timeout-decorator==0.5.0',
         'lxml==4.9.3',
         'timeout-decorator==0.5.0'
     ],
